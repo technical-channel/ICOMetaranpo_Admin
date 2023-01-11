4fe00df0d68e66e4ed09d8b07c7ebd0ec7aff57a
Technical_Channel <52025188+technical-channel@users.noreply.github.com>
2023-01-11 16:36:28 +0530

    Add files via upload

--- src/Config/Contract/Contract.js
+++ src/Config/Contract/Contract.js
@@ -1,6 +1,5 @@
-export const ICOTokenContract = "0xb72FB473aa093D40Ab819E2F682F724e95B9fa79"; // Hammada ico address
+export const ICOTokenContract = "0x0f5F5b69aDfF3B3CE619593785322d60Ddf1d616"; // Hammada ico address
 export const USDTToken = "0x433b5025BE88f9425f6402a2B0701FdDCd0eef8d"; // USDTToken
 export const RanpoToken = "0x5C7614E9371B3Cd836ea3c6d2800967f73Fdc1aB";
-
 
 // 0x5C7614E9371B3Cd836ea3c6d2800967f73Fdc1aB
